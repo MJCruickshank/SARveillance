077c9a7a2d0c7c99e3f689e9f51cc7f051e9c599
Michael Cruickshank <michael.cruickshank@uqconnect.edu.au>
2022-01-11 21:54:45 +0100

    Streamlit webapp for select locations, WIP for custom location

--- webapp.py
+++ webapp.py
@@ -12,17 +12,8 @@ from utils import new_get_image_collection_gif
 st.title('SARveillance')
 st.subheader('Sentinel-1 SAR time series analysis for OSINT use')
 
+
 class SAREXPLORER():
-  scale_bar_dict1 = {
-      "length": 1, 
-      "xy": (0.1, 0.05), 
-      "linewidth": 3,
-      "fontsize": 20,
-      "color": "black",
-      "unit": "km",
-      "ha": "center",
-      "va": "bottom"
-      }
 
   north_arrow_dict1 = {
       "text": "N",
@@ -37,7 +28,6 @@ class SAREXPLORER():
       "va": "center"
       }
 
-
   def __init__(self):
     self.gee = gee
     self.bases = []
@@ -45,19 +35,18 @@ class SAREXPLORER():
     self.dirname = os.path.dirname(__file__)
     self.outpath = self.dirname+"/Data/"
 
-
   def run(self):
     self.auth()
     self.get_bases()
     self.get_collection()
     with st.spinner('Loading timeseries... this may take a couple of minutes'):
-    	self.create_imagery()
+      self.create_imagery()
     st.success('Done!')
     self.display_gif()
     self.show_download()
 
   def auth(self):
-    #self.gee.ee.Authenticate()
+    # self.gee.ee.Authenticate()
     self.gee.ee_initialize()
 
   def get_bases(self):
@@ -65,8 +54,10 @@ class SAREXPLORER():
 
   def get_collection(self):
     collection = ee.ImageCollection('COPERNICUS/S1_GRD')
-    collection_both = collection.filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VV')).filter(ee.Filter.eq('instrumentMode', 'IW'))
-    composite_col = collection_both.map(lambda image: image.select("VH").subtract(image.select("VH")).rename("VH-VV"))
+    collection_both = collection.filter(ee.Filter.listContains(
+        'transmitterReceiverPolarisation', 'VV')).filter(ee.Filter.eq('instrumentMode', 'IW'))
+    composite_col = collection_both.map(lambda image: image.select(
+        "VH").subtract(image.select("VH")).rename("VH-VV"))
     self.col_final = collection_both.map(self.band_adder)
 
   def band_adder(self, image):
@@ -74,9 +65,13 @@ class SAREXPLORER():
     return image.addBands(vh_vv)
 
   def generate_base_aoi(self, base_name):
-    base_gdf = self.bases.loc[self.bases.Name == base_name]
-    latitude = base_gdf.iloc[0]["lat"]
-    longitude = base_gdf.iloc[0]["lon"]
+    if base_name == "Custom Location":
+      latitude = custom_lat
+      longitude = custom_lon
+    else:
+      base_gdf = self.bases.loc[self.bases.Name == base_name]
+      latitude = base_gdf.iloc[0]["lat"]
+      longitude = base_gdf.iloc[0]["lon"]
     base_point = ee.Geometry.Point([float(longitude), float(latitude)])
     base_buffer = base_point.buffer(3000)
     base_bounds = base_buffer.bounds()
@@ -121,7 +116,7 @@ class SAREXPLORER():
       vis_params = visParams,
       region = region,
       fps = 2,
-      mp4 = True,
+      mp4 = False,
       grid_interval = (0.2, 0.2),
       plot_title = base_name,
       date_format = 'YYYY-MM-dd',
@@ -129,7 +124,6 @@ class SAREXPLORER():
       dpi_plot = 100,
       file_format = "png",
       north_arrow_dict = self.north_arrow_dict1,
-      scale_bar_dict = self.scale_bar_dict1,
       verbose = True,
     )
 
@@ -138,32 +132,35 @@ class SAREXPLORER():
     self.generate_timeseries_gif(base_name, start_date, end_date, self.outpath)
 
   def display_gif(self):
-  	gif_loc = os.path.expanduser(self.outpath+"BaseTimeseries/"+base_name+"/"+base_name + ".gif")
-  	file_ = open(gif_loc, "rb")
-  	contents = file_.read()
-  	data_url = base64.b64encode(contents).decode("utf-8")
-  	file_.close()
-  	st.markdown(
-    f'<img src="data:image/gif;base64,{data_url}" alt="Base Timeseries">',
+    gif_loc = os.path.expanduser(self.outpath+"BaseTimeseries/"+base_name+"/"+base_name + ".gif")
+    file_ = open(gif_loc, "rb")
+    contents = file_.read()
+    data_url = base64.b64encode(contents).decode("utf-8")
+    file_.close()
+    st.markdown(
+    f'<img align="left" width="704" height="704" src="data:image/gif;base64,{data_url}" alt="Base Timeseries">',
     unsafe_allow_html=True)
 
   def show_download(self):
-  	gif_loc = os.path.expanduser(self.outpath+"BaseTimeseries/"+base_name+"/"+base_name + ".gif")
-  	with open(gif_loc, "rb") as file:
-  		btn = st.download_button(
-     		label="Download image",
-     		data=file,
-     		file_name="timeseries.gif",
-     		mime="image/gif"
-   			)
+    gif_loc = os.path.expanduser(self.outpath+"BaseTimeseries/"+base_name+"/"+base_name + ".gif")
+    with open(gif_loc, "rb") as file:
+      btn = st.download_button(
+        label="Download image",
+        data=file,
+        file_name="timeseries.gif",
+        mime="image/gif"
+        )
 
 
 if __name__ == '__main__':
-	base_name = st.selectbox('Which location would you like to examine?',('Lesnovka', 'Klintsy', 'Unecha', 'Klimovo Air Base', 'Yelnya', 'Kursk', 'Pogonovo training ground', 'Valuyki', 'Soloti', 'Opuk', 'Bakhchysarai', 'Novoozerne', 'Dzhankoi', 'Novorossiysk', 'Raevskaya'))
-	st.write('You selected:', base_name)
-	start_date= st.text_input('Start Date - use format YYYY-MM-DD', '2021-11-01')
-	end_date = st.text_input('End Date - use format YYYY-MM-DD', '2022-01-10')
-	cartoee.get_image_collection_gif = new_get_image_collection_gif
-	sar = SAREXPLORER()
-	if st.button('Generate SAR Timeseries'):
-		sar.run()
+  base_name = st.selectbox('Which location would you like to examine?',('Custom Location','Lesnovka', 'Klintsy', 'Unecha', 'Klimovo Air Base', 'Yelnya', 'Kursk', 'Pogonovo training ground', 'Valuyki', 'Soloti', 'Opuk', 'Bakhchysarai', 'Novoozerne', 'Dzhankoi', 'Novorossiysk', 'Raevskaya'))
+  st.write('You selected:', base_name)
+  if base_name == "Custom Location":
+    custom_lat = st.text_input('Select Latitude', '')
+    custom_lon = st.text_input('Select Latitude', '')
+  start_date= st.text_input('Start Date - use format YYYY-MM-DD', '2021-11-01')
+  end_date = st.text_input('End Date - use format YYYY-MM-DD', '2022-01-10')
+  cartoee.get_image_collection_gif = new_get_image_collection_gif
+  sar = SAREXPLORER()
+  if st.button('Generate SAR Timeseries'):
+    sar.run()
