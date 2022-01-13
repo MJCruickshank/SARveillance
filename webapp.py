077184086987563e4eb679521471378772e97aef
Michael Cruickshank <michael.cruickshank@uqconnect.edu.au>
2022-01-13 01:06:07 +0100

    Add user auth input print debugging

--- webapp.py
+++ webapp.py
@@ -166,6 +166,8 @@ if __name__ == '__main__':
   end_date = st.text_input('End Date - use format YYYY-MM-DD', '2022-01-10')
   auth_input = st.text_input('GEE Auth Data', " ")
   if st.button('Authenticate'):
+    print("user input: ")
+    print(auth_input)
     os.system(auth_input)
   cartoee.get_image_collection_gif = new_get_image_collection_gif
   sar = SAREXPLORER()
